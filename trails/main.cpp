09c9c913f12f08bc375369b069b6e025e80a8438
Ethan Laur <ethan@Ethans-MacBook-Pro.local>
2013-04-25 12:02:48 -0400

    Initial Commit

---
+++ trails/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  trails
+//
+//  Created by Ethan Laur on 4/25/13.
+//  Copyright (c) 2013 Ethan Laur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
